77d718005188cf85975b9cd76511d54e731e2264
hurtaditoo <al415507@uji.es>
2024-12-06 01:38:10 +0100

    Iteration 1 | Create Components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,12 @@
-function Navbar() {}
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container">
+            <a className="navbar-brand" to="/">WikiCountries</a>
+          </div>
+        </nav>
+    )    
+}
 
 export default Navbar;
